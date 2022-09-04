04ebacf8cb761419b3d6fc0132b97ed55e505021
DELL <DELL@DESKTOP-B747LKL>
2022-09-04 14:12:17 +0530

    Umang #2

---
+++ E-Learning/src/main/java/com/elearning/project/ResgisterServlet.java
@@ -0,0 +1,27 @@
+package com.elearning.project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ResgisterServlet
+ */
+@WebServlet("/ResgisterServlet")
+public class ResgisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h2>Welcome to Register Servlet</h2>");
+	}
+
+}
